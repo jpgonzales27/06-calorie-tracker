ff1314e68518d0d4486195c979ea300c22e16b4e
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-18 19:19:34 -0400

    Creado Componente de formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Form from "./components/Form";
+
 function App() {
   return (
     <>
-      <p className="text-4xl">Contador Calorias</p>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">Contador Calorias</h1>
+        </div>
+      </header>
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form />
+        </div>
+      </section>
     </>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,50 @@
+import { categories } from "../data/categories";
+
+export default function Form() {
+  return (
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">
+          Categoria:
+        </label>
+        <select id="category" className="border border-slate-300 p-2 rounded-lg w-full bg-white">
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">
+          Actividad:
+        </label>
+        <input
+          type="text"
+          id="activity"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">
+          Calorias:
+        </label>
+        <input
+          type="number"
+          id="calories"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-gray-700 hover:bg-gray-900 p-2 uppercase text-white w-full font-bold cursor-pointer"
+        value="Guardar Comida o Guardar Ejercicio"
+      />
+    </form>
+  );
+}
